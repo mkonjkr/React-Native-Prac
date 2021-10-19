bb97f7e77acf3babe31b6a59533f8127f891a6a4
unknown <mkonjkr@gmail.com>
2021-10-18 21:18:14 -0700

    #MAin

--- App.js
+++ App.js
@@ -1,37 +1,7 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Button,
-  Alert,
-} from "react-native";
+import { StyleSheet, Text, View, StatusBar, Image } from "react-native";
+import WelcomeScreen from "./screen/WelcomeScreen.js";
 
 export default function App() {
-  const handlePress = () => console.log("text pressed");
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <Button
-        title="Click"
-        onPress={() =>
-          Alert.alert("Title", "Did you click?", [
-            { text: "Yes", onPress: () => console.log("Yes") },
-            { text: "No", onPress: () => console.log("No") },
-          ])
-        }
-      />
-    </SafeAreaView>
-  );
+  return <WelcomeScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
